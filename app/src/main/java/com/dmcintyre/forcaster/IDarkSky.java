b0350bc6d9d26e8bf87457cbdf22b9b3b368b947
Dan McIntyre <dmcintyre@vectorform.com>
2018-08-13 21:51:50 -0400

    Minor changes

--- app/src/main/java/com/dmcintyre/forcaster/IDarkSky.java
+++ app/src/main/java/com/dmcintyre/forcaster/IDarkSky.java
@@ -6,6 +6,9 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 
+/**
+ * Define methods to access the Dark Sky api's to retrieve weather information.
+ */
 public interface IDarkSky {
     @GET("{latitude},{longitude}")
     Call<Forecast> getForecast(@Path("latitude") double latitude, @Path("longitude") double longitude);
